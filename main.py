b295e54a93a348e0b0f8d9dbd8b69cb65a189e27
Kaustubh <saikaustubh10@gmail.com>
2021-01-18 13:26:51 +0530

    capturing packets

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 #!/usr/bin/env/ python
 import scapy.all as scapy
+from scapy.layers import http
 
 
 def sniff(interface):
     #setting store to False wont store the packets in the computer's local storage
-    scapy.sniff(iface=interface, store=False, prn=process_sniffed_packet)
+    scapy.sniff(iface=interface, store=False, prn=process_sniffed_packet, filter="udp")
 
 
 def process_sniffed_packet(packet):
-    print(packet)
+    if packet.haslayer(http.HTTPRequest):
+        print(packet)
 
 
 sniff("eth0")
